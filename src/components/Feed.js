e805a5bc1d41796f75f44d699f07850431554c78
Maggie von Ebers <vonebersm@gmail.com>
2019-02-23 16:08:37 -0600

    bubbles and feeds

---
+++ src/components/Feed.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+
+class Feed extends React.Component {
+    renderBubble(i) {
+        return <Bubble />
+    }
+}
