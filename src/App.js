73a250334234025da718b882b6f7bb209b4a54e3
Katerina Boyer <katerinaboyer@cometnet-10-21-28-136.utdallas.edu>
2019-02-23 15:21:13 -0600

    redo try2

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    state = {
+        number: 1
+ }
+
+// this is where function for button goes 
+function timer() {
+    var myTimer = setInterval(myClock, 1000);
+    var c = 5;
+
+    function myClock() {
+        document.getElementById("demo").innerHTML = --c;
+        if (c==0) {
+            clearInterval(myTimer);
+            alert("Times Up!");
+        }
+
+    }
+
+}
+
+
+  render() {
+    return (
+      <div className="App">
+        Hello World
+        <button onclick = "clock()"> Start a timer </button>
+        <button onclick = "clearInterval(myTimer)"> Pause Timer </button>
+      </div>
+    );
+  }
+}
+
+export default App;
